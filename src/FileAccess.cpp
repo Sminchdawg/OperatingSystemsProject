1195d512cdec95c65b45512942f53b08f2d81f88
Sminchdawg <magenova12@gmail.com>
2020-05-03 13:44:18 -0400

    Got allocate up and running

--- src/FileAccess.cpp
+++ src/FileAccess.cpp
@@ -22,7 +22,7 @@ int32_t fetchBlockFromFile(struct Ext2File *f, struct Inode *i, uint32_t bNum, v
 
             goto direct;
         }
-    } else if (bNum < 12 + k + pow(k, 2)) {
+    } else if (bNum < 12 + k + k * k) {
         cout << "The block is in the first double indirect block" << endl;
         // ^^ Index is in first double indirect block, aka i_block[13]
         if (i->i_block[13] == 0) {
@@ -42,11 +42,11 @@ int32_t fetchBlockFromFile(struct Ext2File *f, struct Inode *i, uint32_t bNum, v
         } else {
             fetchBlock(f, i->i_block[14], blockList);
 
-            long kPow = pow(k,2);
+            long kPow = k * k;
 
             bNum -= (12 + k + kPow);
 
-            index = bNum / pow(k, 2);
+            index = bNum / (k * k);
             bNum = bNum % kPow;
 
             if (blockList[index] == 0) {
@@ -82,7 +82,7 @@ int32_t fetchBlockFromFile(struct Ext2File *f, struct Inode *i, uint32_t bNum, v
     }
 }
 
-int32_t writeBlockToFile(struct Ext2File *f, struct Inode *i, uint32_t bNum, void *buf) {
+int32_t writeBlockToFile(struct Ext2File *f, struct Inode *i, uint32_t bNum, void *buf, uint32_t iNum) {
     // Write the given buffer into the block bNum in the file
     // this may involve allocating one or more data blocks
     int k = f->file_system_block_size / 4;
@@ -94,59 +94,58 @@ int32_t writeBlockToFile(struct Ext2File *f, struct Inode *i, uint32_t bNum, voi
 
     if (bNum < 12) {
         if (i->i_block[bNum] == 0) {
-            //i->i_block;
-            // i_block[bNum] = bNum; // allocate
-            // writeInode(f, iNum, buf); // figure out what is being written here
-        } else {
-            blockList = i->i_block;
-            goto direct;
+            i->i_block[bNum] = allocate(f, -1);
+            writeInode(f, iNum, i);
         }
+
+        blockList = i->i_block;
+        goto direct;
     } else if (bNum < 12 + k) {
         cout << "The block is in the first single indirect block in write" << endl;
         if (i->i_block == 0) {
-            // i_block[12] // allocate
-            // writeInode(f, iNum, buf); // figure out what is being witten here
-        } else {
-            fetchBlock(f, i->i_block[12], temp);
-            ibNum = i->i_block[12];
-            blockList = temp;
-            bNum -= 12;
-
-            goto direct;
+            i->i_block[12] = allocate(f, -1);
+            writeInode(f, iNum, i);
         }
-    } else if (bNum < 12 + k + pow(k, 2)) {
+
+        fetchBlock(f, i->i_block[12], temp);
+        ibNum = i->i_block[12];
+        blockList = temp;
+        bNum -= 12;
+
+        goto direct;
+    } else if (bNum < 12 + k + k * k) {
         cout << "The block is in the first double indirect block in write" << endl;
         if (i->i_block[13] == 0) {
-            // i_block[13] // allocate
-            // writeInode(f, iNum, buf); // figure out what is being witten here
-        } else {
-            fetchBlock(f, i->i_block[13], temp);
-
-            ibNum = i->i_block[13];
-            blockList = temp;
-            bNum -= (12 + k);
-
-            goto single;
+            i->i_block[13] = allocate(f, -1);
+            writeInode(f, iNum, i);
         }
+
+        fetchBlock(f, i->i_block[13], temp);
+
+        ibNum = i->i_block[13];
+        blockList = temp;
+        bNum -= (12 + k);
+
+        goto single;
     } else {
         cout << "The block is in the first triple indirect block in write" << endl;
 
         if (i->i_block[14] == 0) {
-            // i_block[14] // allocate
-            // writeInode(f, iNum, buf); // figure out what is being witten here
-        } else {
-            fetchBlock(f, i->i_block[14], temp);
-
-            ibNum = i->i_block[14];
-            blockList = temp;
-
-            long kPow = pow(k,2);
-
-            bNum -= (12 + k + kPow);
-
-            index = bNum / pow(k, 2);
-            bNum = bNum % kPow;
+            i->i_block[14] = allocate(f, -1);
+            writeInode(f, iNum, i);
         }
+
+        fetchBlock(f, i->i_block[14], temp);
+
+        ibNum = i->i_block[14];
+        blockList = temp;
+
+        long kPow = k * k;
+
+        bNum -= (12 + k + kPow);
+
+        index = bNum / (k * k);
+        bNum = bNum % kPow;
     }
 
     single:
@@ -154,33 +153,56 @@ int32_t writeBlockToFile(struct Ext2File *f, struct Inode *i, uint32_t bNum, voi
     bNum = bNum % k;
 
     if (blockList[index] == 0) {
-        // blockList[index] // allocate
-        // writeBlock(f, bNum, buf);
-    } else {
-        ibNum = blockList[index];
-        fetchBlock(f, blockList[index], temp);
-        blockList = temp;
+        blockList[index] = allocate(f, -1);
+        writeBlock(f, bNum, i);
     }
+    ibNum = blockList[index];
+    fetchBlock(f, blockList[index], temp);
+    blockList = temp;
 
     direct:
     if (blockList[bNum] == 0) {
-        // blockList[bNum] // allocate
-        // writeBlock(f, bNum, buf);
-    } else {
-        cout << "Made it to writeBlock" << endl;
-        writeBlock(f, blockList[bNum], buf);
+        blockList[bNum] = allocate(f, -1);
+        writeBlock(f, bNum, i);
     }
+
+    cout << "Made it to writeBlock" << endl;
+    writeBlock(f, blockList[bNum], buf);
 
     return 0;
 }
 
-int32_t allocate(Ext2File* f, uint32_t blockGroup) {
-/*
+int32_t allocate(Ext2File* f, int32_t blockGroup) {
+    int32_t firstGroup = 0;
+    int32_t lastGroup = f->num_block_groups -1;
+    uint8_t* tempBlock = new uint8_t[f->file_system_block_size];
     uint32_t blockNum;
-    f->superBlock->s_free_blocks_count--;
-    f->bgdt[0]->blockGroups.bg_free_blocks_count--;
-    writeSuperblock(f, 0, f->superBlock);
-    writeBGDT(f, 0, f->bgdt[0]->blockGroups);
-    return blockNum;
-    */
+
+    if (blockGroup >= 0) {
+        firstGroup = blockGroup;
+        lastGroup = blockGroup;
+    }
+
+    for (int i = firstGroup; i <= lastgroup; i++) {
+        fetchBlock(f, f->bgdt->blockGroups[i], tempBlock);
+
+        for (int j = 0; j < f->file_system_block_size; j++) {
+            if (tempBlock[j] != 0xff) {
+                for (int k = 0; k < 8; k++) {
+                    if (tempBlock[j] & (1 << k)) {
+                        blockNum = k + 8 * j + i * f->superBlock->s_blocks_per_group + f->superBlock->s_first_data_block;
+                        tempBlock[j] |= 1 << k;
+                        writeBlock(f, f->bgdt->blockGroups[i], tempBlock);
+                        f->bgdt->blockGroups[i].bg_free_blocks_count--;
+                        f->superBlock->s_free_blocks_count--;
+                        writeSuperblock(f, 0, f->superBlock);
+                        writeBGDT(f, 0, f->bgdt->blockGroups[0]);
+                        return blockNum;
+                    }
+                }
+            }
+        }
+    }
+
+    return 0;
 }
